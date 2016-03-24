962b3b634c41a9db833ca87bc72fb0df23f7c2ea
alvaropeon <alvaro_peon@hotmail.com>
2016-03-24 13:40:19 -0700

    Adding dockerfile and fixing url

--- app/app.settings.js
+++ app/app.settings.js
@@ -1,5 +1,5 @@
 var app = angular.module('hpsa-client');
 
 app.constant('AppSettings', {
-   apiUrl: 'http://expense-api.52.37.93.140.nip.io/',
+   apiUrl: 'http://expense-api.52.38.36.91.nip.io/',
 })
